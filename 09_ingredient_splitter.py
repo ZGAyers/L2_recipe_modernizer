be22e6b786eefe05f666389436580ae830ba4bcc
Zarah70879 <51766656+Zarah70879@users.noreply.github.com>
2020-02-20 09:51:40 +1300

    End of day work - Partially done ingredient splitter part 2

--- 09_ingredient_splitter.py
+++ 09_ingredient_splitter.py
@@ -8,6 +8,17 @@ mixed_regex = "\d{1,3}\s\d{1,3}\/\d{1,3}"
 
 if re.match(mixed_regex, recipe_line):
     print("true")
+    # Get mixed number by matching regex
+    pre_mixed_num = re.match(mixed_regex, recipe_line)
+    mixed_num = pre_mixed_num.group()
 
-else:
-    print("false")
+    # Replace space with a + sign...
+    amount = mixed_num.replace(" ", "+")
+    # Change the string into a decimal
+    amount = eval(amount)
+    print(amount)
+
+    # Get unit and ingredient
+    compile_regrex = re.compile(mixed_regex)
+    print(compile_regrex)
+    unit_ingredient = re.split(compile_regrex, recipe_line)
