77109ce7cf9b772a33ed1c67561b85f38b765180
Zarah70879 <51766656+Zarah70879@users.noreply.github.com>
2020-02-20 09:44:20 +1300

    recipe moderniser not working but part 1 of ingredient splitter is

--- 09_recipe_moderniser.py
+++ 09_recipe_moderniser.py
@@ -5,6 +5,7 @@ import csv
 
 # --- Functions ---
 
+
 # Not Blank function goes here
 def not_blank(question, error_msg, num_ok):
     error = error_msg
@@ -32,6 +33,7 @@ def not_blank(question, error_msg, num_ok):
         else:
             return response
 
+
 # Number Checking Function goes here
 def num_check(question):
 
@@ -51,16 +53,90 @@ def num_check(question):
             print(error)
 
 
+def general_converter(how_much, lookup, dictionary, conversion_factor):
+    # if unit is in dictionary, convert to mL
+    if lookup in dictionary:
+        mult_by = dictionary.get(unit)
+        how_much = how_much * mult_by * conversion_factor
+
+    return how_much
+
+
+def unit_checker():
+
+    unit_tocheck = input("Unit? ")
+
+    # Abbreviation lists
+    teaspoon = ["tsp", "teaspoon", "t"]
+    tablespoon = ["tbs", "tablespoon", "T", "tbsp"]
+    cup = ["cup", "c", "cups"]
+    ounce = ["oz", "ounces", "fl oz", "ounce"]
+    pint = ["p", "pt", "fl pt", "pint", "pints"]
+    quart = ["q", "qt", "fl qt", "quart", "quarts"]
+    mls = ["ml", "milliliter", "millilitre", "milliliters", "millilitres"]
+    litre = ["litre", "liter", "l", "litres", "liters"]
+    pound = ["pound", "lb", "#", "pounds"]
+    grams = ["g", "gram", "grams"]
+
+    if unit_tocheck == "":
+        print("you chose {}".format(unit_tocheck))
+        return unit_tocheck
+
+    elif unit_tocheck.lower() in grams:
+        return "g"
+    elif unit_tocheck == "T" or unit_tocheck.lower() in tablespoon:
+        return "tbs"
+    elif unit_tocheck.lower() in teaspoon:
+        return "tsp"
+    elif unit_tocheck.lower() in cup:
+        return "cup"
+    elif unit_tocheck.lower() in ounce:
+        return "ounce"
+    elif unit_tocheck.lower() in pint:
+        return "pint"
+    elif unit_tocheck.lower() in quart:
+        return "quart"
+    elif unit_tocheck.lower() in mls:
+        return "mls"
+    elif unit_tocheck.lower() in litre:
+        return "litre"
+    elif unit_tocheck.lower() in pound:
+        return "pound"
+
+
 # -- Main Routine --
 
 # set up Dictionaries
+# open food dictionary
+groceries = open('01_ingredients_ml_to_g.csv')
+# Read data into a list
+csv_groceries = csv.reader(groceries)
+# Create a dictionary to hold the data
+food_dictionary = {}
 
+# Add the data from the list into the dictionary
+# (first item in row is key, next is definition)
+
+for row in csv_groceries:
+    food_dictionary[row[0]] = row[1]
 # set up list to hold 'modernised' ingredients
+unit_central = {
+    "tsp": 5,
+    "tbs": 15,
+    "cup": 237,
+    "ounce": 28.35,
+    "pint": 473,
+    "quart": 946,
+    "pound": 454,
+    "litre": 1000,
+    "ml": 1
+}
 
 # Ask user for recipe name and check its not blank
 recipe_name = not_blank("What is the recipe name? ",
                    "The recipe can't be blank and can't contain numbers, ",
                    "no")
+
 # Ask user where the recipe is originally from (numbers ok)
 source = not_blank("Where is the recipe from? ",
                    "The recipe source can't be blank, ",
@@ -91,13 +167,36 @@ while dodgy_sf == "yes":
 
 # Loop for each ingredient
 keep_going = ""
-while keep_going != "xxx":
-    # Get ingredient amount
+while keep_going == "":
     # Get ingredient name
+    ingredient = not_blank("Please enter a ingredient name: ",
+                           "This can't be blank",
+                           "yes")
+    if ingredient.lower() == "xxx":
+        break
+    # Get ingredient amount
+    amount = num_check("What is the amount of this ingredient? ")
+
     # Get unit
+    unit = unit_checker()
+
     # Convert unit to ml
+    amount = general_converter(amount, unit, unit_central, 1)
+    print(amount)
+
     # Convert from ml to g
+    if ingredient in food_dictionary:
+        mult_by = food_dictionary.get(ingredient)
+        amount = amount * float(mult_by) / 250
+        print("Amount in g {}".format(amount))
+
+    # if no ingredient give / ingredient is unknown, leave as is
+    else:
+        print("{} is unchanged".format(amount))
+
     # Put updates ingredient in list
+        print("Amount in mL {}".format(amount))
+        print("Amount in g {}".format(amount))
 
 
 # Output ingredient list
